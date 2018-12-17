5de2e3d1accd82bd171455f361a6b316d03a4441
aramkoukia <aramkoukia@gmail.com>
2018-12-16 22:45:42 -0800

    added olumns

--- ecommerce/ecommerce/Product.sql
+++ ecommerce/ecommerce/Product.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
 	[ProductId] INT NOT NULL PRIMARY KEY, 
-    [ProductCode] NVARCHAR(150) NOT NULL, 
+    [ProductCode] NVARCHAR(150) NOT NULL,
+    [Barcode] NVARCHAR(150) NOT NULL,
     [ProductName] NVARCHAR(250) NOT NULL, 
     [ProductTypeId] INT NULL, 
+	[SalesPrice] Decimal(18, 2) NULL, 
+	[PurchasePrice] Decimal(18, 2) NULL, 
     [CreatedDate] DATETIME NULL, 
     [ModifiedDate] DATETIME NULL, 
     CONSTRAINT [FK_Product_ToProductType] FOREIGN KEY (ProductTypeId) REFERENCES [ProductType]([ProductTypeId])
