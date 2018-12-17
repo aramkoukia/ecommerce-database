4c676f53ff677fec7f3df6cfce10af90e4fada7e
aramkoukia <aramkoukia@gmail.com>
2018-12-16 22:21:43 -0800

    added ouple tables

---
+++ ecommerce/ecommerce/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Code] NVARCHAR(150) NULL, 
+    [ProductName] NVARCHAR(250) NULL, 
+    [ProductTypeId] NCHAR(10) NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [ModifiedDate] DATETIME NULL
+)
---
+++ ecommerce/ecommerce/ProductType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ProductType]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [TypeName] NVARCHAR(150) NOT NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [ModifiedDate] DATETIME NULL
+)
