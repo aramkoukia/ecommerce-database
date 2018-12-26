66537d9bbcb70d4a05231d6e8c71d01f2c0fab6f
Aram Koukia <aramkoukia@gmail.com>
2018-12-25 17:26:02 -0800

    fixed allow null

--- ecommerce/ecommerce/OrderPayment.sql
+++ ecommerce/ecommerce/OrderPayment.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[OrderPayment]
 (
 	[OrderPaymentId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
-	[OrderId] INT NULL, 
+	[OrderId] INT NOT NULL, 
     [PaymentAmount] DECIMAL(18, 2) NOT NULL, 
     [PaymentDate] DATETIME NOT NULL, 
 	[PaymentTypeId] INT NOT NULL,
