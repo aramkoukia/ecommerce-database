84058702ab2eeb862c79c5b793d4ad90e2400227
Aram Koukia <aramkoukia@gmail.com>
2018-12-28 11:46:18 -0800

    some db changes

--- ecommerce/ecommerce/ProductInventoryHistory.sql
+++ ecommerce/ecommerce/ProductInventoryHistory.sql
@@ -5,8 +5,9 @@
     [LocationId] INT NOT NULL,
     [Balance] decimal(18,2) NOT NULL,
     [ModifiedDate] DATETIME NOT NULL DEFAULT getdate(),
-	[BinCode] NVARCHAR(100) NULL,
-	[CreatedByUserId] NVARCHAR(450) NULL,
+	[BinCode] NVARCHAR(100) NOT NULL,
+	[CreatedByUserId] NVARCHAR(450) NOT NULL,
+    [Notes] NVARCHAR(450) NOT NULL, 
     CONSTRAINT [FK_ProductInventoryHistory_ToProduct] FOREIGN KEY (ProductId) REFERENCES [Product]([ProductId]),
 	CONSTRAINT [FK_ProductInventoryHistory_ToLocation] FOREIGN KEY (LocationId) REFERENCES [Location]([LocationId])
 )
