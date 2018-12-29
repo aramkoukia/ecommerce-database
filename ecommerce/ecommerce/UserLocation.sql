4d09d4722e537cf6e59b32a96d25376a4754bb2d
Aram Koukia <aramkoukia@gmail.com>
2018-12-29 11:55:15 -0800

    added user location pk

--- ecommerce/ecommerce/UserLocation.sql
+++ ecommerce/ecommerce/UserLocation.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[UserLocation]
 (
+    [UserLocationId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
 	[UserId] [nvarchar](450) NOT NULL,
 	[LocationId] int NOT NULL,
- CONSTRAINT [PK_UserLocation] PRIMARY KEY CLUSTERED ([UserId] ASC, [LocationId] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
  CONSTRAINT [FK_UserLocation_Location_LocationId] FOREIGN KEY([LocationId]) REFERENCES [dbo].[Location] ([LocationId]) ,
  CONSTRAINT [FK_UserLocation_Users_UserId] FOREIGN KEY([UserId]) REFERENCES [dbo].[Users] ([Id])
 )
