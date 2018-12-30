7d30c5036ef77a15638e3f83a036087cbbd12d49
Aram Koukia <aramkoukia@gmail.com>
2018-12-30 12:57:20 -0800

    added purchase tables

---
+++ ecommerce/ecommerce/Purchase.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Purchase]
+(
+	[PurchaseId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+	[Supplier] NVARCHAR(100) NULL, 
+    [PurchaseDate] DATETIME NOT NULL, 
+    [Total] DECIMAL(18, 2) NOT NULL, 
+    [SubTotal] DECIMAL(18, 2) NOT NULL, 
+    [Notes] NVARCHAR(500) NULL, 
+	[DeliveryDate] DATETIME NOT NULL,
+    [CreatedDate] DATETIME NOT NULL, 
+    [CreatedByUserId] NVARCHAR(450) NOT NULL,
+)
---
+++ ecommerce/ecommerce/PurchaseDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[OrderPurchase]
+(
+	[PurchaseDetailId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [PurchaseId] INT NOT NULL, 
+	[ProductId] INT NOT NULL, 
+    [Amount] DECIMAL(18, 2) NOT NULL, 
+    [UnitPrice] DECIMAL(18, 2) NOT NULL, 
+    [TotalPrice] DECIMAL(18, 2) NOT NULL, 
+    CONSTRAINT [FK_PurchaseDetail_ToPurchase] FOREIGN KEY ([PurchaseId]) REFERENCES [Purchase]([PurchaseId]),
+	CONSTRAINT [FK_Product_ToPurchase] FOREIGN KEY ([ProductId]) REFERENCES [Product]([ProductId])
+)
