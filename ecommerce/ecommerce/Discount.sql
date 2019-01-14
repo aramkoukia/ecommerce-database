3525f5c07c4a0e37ce62fe753e454597824e4bad
Aram Koukia <aramkoukia@gmail.com>
2019-01-13 22:32:40 -0800

    added discount table

---
+++ ecommerce/ecommerce/Discount.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Discount]
+(
+	[DiscountId] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
+    [DiscountName] NVARCHAR(50) NOT NULL, 
+	[CustomerGrade] NVARCHAR(10) NOT NULL,
+    [Percentage] DECIMAL(18, 3) NOT NULL,
+    [ModifiedDate] DATETIME NOT NULL DEFAULT getdate()
+)
