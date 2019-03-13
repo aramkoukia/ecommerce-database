0c2cd80180f9fe6a6b00b08584816625f4b9ede7
aramkoukia <aramkoukia@gmail.com>
2019-03-12 19:28:38 -0700

    adding customer account table

---
+++ ecommerce/ecommerce/CustomerAccount.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CustomerAccount]
+(
+	[CustomerAccountId] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	[CustomerId] INT NOT NULL,
+	[Amount] DECIMAL NOT NULL,
+	[Notes] NVARCHAR(500) NULL,
+    [CreatedDate] DATETIME NOT NULL, 
+    [CreatedByUserId] NVARCHAR(450) NOT NULL,
+)
