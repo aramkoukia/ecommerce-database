b86fa0cf10bc4383077da306629e697ba397fbcd
aramkoukia <aramkoukia@gmail.com>
2019-03-29 13:37:39 -0700

    added purchase detail fields

--- ecommerce/ecommerce/PurchaseDetail.sql
+++ ecommerce/ecommerce/PurchaseDetail.sql
@@ -6,6 +6,12 @@
     [Amount] DECIMAL(18, 2) NOT NULL, 
     [UnitPrice] DECIMAL(18, 2) NOT NULL, 
     [TotalPrice] DECIMAL(18, 2) NOT NULL, 
+    [Status] NVARCHAR(50) NULL,  
+    [EstimatedDelivery] DATETIME NULL, 
+    [ArrivedAtLocationId] INT NULL, 
+    [ArrivedDate] DATETIME NULL,
+    [CreatedDate] DATETIME NULL, 
+    [CreatedByUserId] NVARCHAR(450) NULL,	
     CONSTRAINT [FK_PurchaseDetail_ToPurchase] FOREIGN KEY ([PurchaseId]) REFERENCES [Purchase]([PurchaseId]),
 	CONSTRAINT [FK_Product_ToPurchase] FOREIGN KEY ([ProductId]) REFERENCES [Product]([ProductId])
 )
