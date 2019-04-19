b8216b71da7adcc5586f7f58750480e99c899237
aramkoukia <aramkoukia@gmail.com>
2019-04-19 11:50:17 -0700

    added location fields

--- ecommerce/ecommerce/Location.sql
+++ ecommerce/ecommerce/Location.sql
@@ -2,5 +2,9 @@
 (
 	[LocationId] INT NOT NULL PRIMARY KEY, 
     [LocationName] NVARCHAR(50) NOT NULL, 
-    [LocationAddress] NVARCHAR(500) NULL
+    [LocationAddress] NVARCHAR(500) NULL, 
+    [Country] NVARCHAR(100) NULL, 
+    [Province] NVARCHAR(100) NULL, 
+    [PostalCode] NVARCHAR(100) NULL, 
+    [PhoneNumber] NCHAR(10) NULL
 )
