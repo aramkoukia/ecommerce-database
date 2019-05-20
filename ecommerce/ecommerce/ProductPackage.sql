0cf9250f039a7068d8202851d55493aeb9058450
aramkoukia <aramkoukia@gmail.com>
2019-05-20 15:02:37 -0700

    changed column

--- ecommerce/ecommerce/ProductPackage.sql
+++ ecommerce/ecommerce/ProductPackage.sql
@@ -4,7 +4,7 @@
 	[ProductId] INT NOT NULL, 
     [Package] NVARCHAR(150) NOT NULL,
     [PackagePrice] decimal(20,2) NOT NULL,
-    [MainPackageMultiplier] decimal(20,3) NOT NULL,
+    [AmountInMainPackage] decimal(20,3) NOT NULL,
     [ModifiedDate] DATETIME NOT NULL DEFAULT getdate(),
     CONSTRAINT [FK_ProductPackage_ToProduct] FOREIGN KEY (ProductId) REFERENCES [Product]([ProductId])
 )
