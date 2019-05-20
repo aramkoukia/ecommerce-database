f446d87b07d0ee910ea12b56d00d6abc6ff01d5b
aramkoukia <aramkoukia@gmail.com>
2019-05-20 12:40:01 -0700

    added package table

---
+++ ecommerce/ecommerce/ProductPackage.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProductPackage]
+(
+    [ProductPackageId] INT NOT NULL PRIMARY KEY,
+	[ProductId] INT NOT NULL, 
+    [Package] NVARCHAR(150) NOT NULL,
+    [PackagePrice] decimal(20,2) NOT NULL,
+    [ModifiedDate] DATETIME NOT NULL DEFAULT getdate(),
+    CONSTRAINT [FK_ProductPackage_ToProduct] FOREIGN KEY (ProductId) REFERENCES [Product]([ProductId])
+)
