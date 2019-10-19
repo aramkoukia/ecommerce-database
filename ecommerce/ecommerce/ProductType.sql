def14d2774dc0cf8a12fb8f64b8c03df629d2f37
aramkoukia <aramkoukia@gmail.com>
2019-10-19 14:57:33 -0700

    updating data type

--- ecommerce/ecommerce/ProductType.sql
+++ ecommerce/ecommerce/ProductType.sql
@@ -2,5 +2,5 @@
 (
 	[ProductTypeId] INT NOT NULL PRIMARY KEY, 
     [ProductTypeName] NVARCHAR(150) NOT NULL, 
-    [ModifiedDate] DATETIME NOT NULL DEFAULT getdate()
+    [ModifiedDate] DATETIME2 NOT NULL DEFAULT getdate()
 )
