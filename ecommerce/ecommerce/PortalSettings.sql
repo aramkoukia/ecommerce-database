6975280b0e57d44cfea85cfc62b767ca1e994956
Aram Koukia <aramkoukia@gmail.com>
2020-04-17 15:37:27 -0700

    adding portal setting

---
+++ ecommerce/ecommerce/PortalSettings.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PortalSettings]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[PortalTitle] NVARCHAR(100) NOT NULL,
+	[SidebarImageurl] NVARCHAR(500) NOT NULL,
+	[LogoImageurl] NVARCHAR(500) NOT NULL
+)
