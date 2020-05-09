001cc9e9d87f219ffdf84ffe9723de76a24878aa
Aram Koukia <aramkoukia@gmail.com>
2020-05-09 10:33:41 -0700

    added product type website fields

--- ecommerce/ecommerce/ProductType.sql
+++ ecommerce/ecommerce/ProductType.sql
@@ -2,5 +2,8 @@
 (
 	[ProductTypeId] INT NOT NULL PRIMARY KEY, 
     [ProductTypeName] NVARCHAR(150) NOT NULL, 
-    [ModifiedDate] DATETIME2 NOT NULL DEFAULT getdate()
+    [ModifiedDate] DATETIME2 NOT NULL DEFAULT getdate(), 
+    [ThumbnailImagePath] NVARCHAR(500) NULL, 
+    [HeaderImagePath] NVARCHAR(500) NULL, 
+    [Description] NVARCHAR(MAX) NULL
 )
