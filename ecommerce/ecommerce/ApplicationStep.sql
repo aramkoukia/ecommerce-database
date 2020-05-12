d0c092ece9f7dcb9c9db14fca658be10fe8f50e6
Aram Koukia <aramkoukia@gmail.com>
2020-05-12 11:04:28 -0700

    added application tables

---
+++ ecommerce/ecommerce/ApplicationStep.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ApplicationStep]
+(
+	[StepId] INT NOT NULL PRIMARY KEY,
+    [StepTitle] NVARCHAR(500) NOT NULL,
+    [StepDescription] NVARCHAR(MAX) NULL, 
+    [IsRangeValue] BIT NOT NULL,
+    [MinValue] NVARCHAR(100) NULL,
+    [MaxValue] NVARCHAR(100) NULL,
+    [ValueUnit] NVARCHAR(100) NOT NULL,
+    [SortOrder] INT NOT NULL,
+)
---
+++ ecommerce/ecommerce/ApplicationStepDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ApplicationStepDetail]
+(
+	[StepDetailId] INT NOT NULL PRIMARY KEY, 
+    [StepId] INT NOT NULL, 
+    [StepDetailTitle] NVARCHAR(500) NOT NULL, 
+    [StepDetailDescription] NVARCHAR(MAX) NULL, 
+    [ThumbnailImagePath] NVARCHAR(500) NULL, 
+    [SortOrder] INT NOT NULL,
+    CONSTRAINT [FK_ApplicationStepDetail_ToApplicationStep] FOREIGN KEY (StepId) REFERENCES [ApplicationStep]([StepId]),
+)
