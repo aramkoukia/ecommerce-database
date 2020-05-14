cb1c669af7d20b1e91476774f7f01dad7d904a29
Aram Koukia <aramkoukia@gmail.com>
2020-05-14 11:06:44 -0700

    added product website fields

---
+++ ecommerce/ecommerce/ProductWebsite.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ProductWebsite]
+(
+	[ProductWebsiteId] INT NOT NULL PRIMARY KEY, 
+    [ProductId] INT NOT NULL,
+	[Detail] NVARCHAR(max) NULL, 
+	[Description] NVARCHAR(Max) NULL, 
+	[WarrantyInformation] NVARCHAR(Max) NULL,
+	[AdditionalInformation] NVARCHAR(Max) NULL,
+	[UserManualPath] NVARCHAR(500) NULL,
+    CONSTRAINT [FK_ProductWebsite_ToProduct] FOREIGN KEY (ProductId) REFERENCES [Product]([ProductId])
+)
---
+++ ecommerce/ecommerce/ProductWebsiteImage.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ProductWebsiteImage]
+(
+	[ProductWebsiteImageId] INT NOT NULL PRIMARY KEY, 
+    [ProductId] INT NOT NULL,
+	[ImagePath] NVARCHAR(500) NULL, 
+    CONSTRAINT [FK_ProductWebsiteImage_ToProduct] FOREIGN KEY (ProductId) REFERENCES [Product]([ProductId])
+)
