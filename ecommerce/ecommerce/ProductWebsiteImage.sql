dac503cbdeca18a034a5f9b24add5da69db57160
Aram Koukia <aramkoukia@gmail.com>
2020-05-21 10:33:06 -0700

    added large image and small image files

--- ecommerce/ecommerce/ProductWebsiteImage.sql
+++ ecommerce/ecommerce/ProductWebsiteImage.sql
@@ -3,5 +3,8 @@
 	[ProductWebsiteImageId] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
     [ProductId] INT NOT NULL,
 	[ImagePath] NVARCHAR(500) NULL, 
+	[SmallImagePath] NVARCHAR(500) NULL, 
+	[LargeImagePath] NVARCHAR(500) NULL, 
+	[ImageSize] NVARCHAR(500) NULL, 
     CONSTRAINT [FK_ProductWebsiteImage_ToProduct] FOREIGN KEY (ProductId) REFERENCES [Product]([ProductId])
 )
