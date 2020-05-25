8f4102ef5c898b16fb0faca46693bb867bc28070
Aram Koukia <aramkoukia@gmail.com>
2020-05-25 10:29:03 -0700

    fixing reference

--- ecommerce/ecommerce/ApplicationStepDetail.sql
+++ ecommerce/ecommerce/ApplicationStepDetail.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[ApplicationStepDetail]
 (
 	[ApplicationStepDetailId] INT NOT NULL PRIMARY KEY, 
-    [StepId] INT NOT NULL, 
+    [ApplicationStepId] INT NOT NULL, 
     [StepDetailTitle] NVARCHAR(500) NOT NULL, 
     [StepDetailDescription] NVARCHAR(MAX) NULL, 
     [ThumbnailImagePath] NVARCHAR(500) NULL, 
     [SortOrder] INT NOT NULL,
-    CONSTRAINT [FK_ApplicationStepDetail_ToApplicationStep] FOREIGN KEY (StepId) REFERENCES [ApplicationStep]([StepId]),
+    CONSTRAINT [FK_ApplicationStepDetail_ToApplicationStep] FOREIGN KEY (ApplicationStepId) REFERENCES [ApplicationStep](ApplcationStepId),
 )
