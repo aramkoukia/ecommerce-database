e30a53682e2adf251421a818a0a50078b3164776
somayehgh <somayehghazvinian@gmail.com>
2020-06-20 11:40:41 -0700

    adding new fk

--- ecommerce/ecommerce/ApplicationStepDetailTag.sql
+++ ecommerce/ecommerce/ApplicationStepDetailTag.sql
@@ -4,4 +4,5 @@
     [ApplicationStepDetailId] INT NOT NULL,
 	[TagId] INT NOT NULL,
     CONSTRAINT [FK_ApplicationStepDetailTag_ToApplicationStepDetail] FOREIGN KEY (ApplicationStepDetailId) REFERENCES [ApplicationStepDetail](ApplicationStepDetailId),
+    CONSTRAINT [FK_ApplicationStepDetailTag_ToTag] FOREIGN KEY (TagId) REFERENCES [Tag](TagId),
 )
