a1c2315c8c073b83aeea5f55b5d224ec221a9185
somayehgh <somayehghazvinian@gmail.com>
2020-06-20 11:32:47 -0700

    new table

---
+++ ecommerce/ecommerce/ApplicationStepDetailTag.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ApplicationStepDetailTag]
+(
+	[ApplicationStepDetailTagId] INT NOT NULL PRIMARY KEY, 
+    [ApplicationStepDetailId] INT NOT NULL,
+	[TagId] INT NOT NULL,
+    CONSTRAINT [FK_ApplicationStepDetailTag_ToApplicationStepDetail] FOREIGN KEY (ApplicationStepDetailId) REFERENCES [ApplicationStepDetail](ApplicationStepId),
+)
