3797c99524d9b46af320ae9fd128201db046c1a0
somayehgh <somayehghazvinian@gmail.com>
2020-07-13 20:45:14 -0700

    added new columns

--- ecommerce/ecommerce/PortalSettings.sql
+++ ecommerce/ecommerce/PortalSettings.sql
@@ -4,5 +4,10 @@
 	[PortalTitle] NVARCHAR(100) NOT NULL,
 	[SidebarImageUrl] NVARCHAR(500) NOT NULL,
 	[LogoImageUrl] NVARCHAR(500) NOT NULL, 
-    [ShowTitleOnSideBar] BIT NOT NULL Default 1
+    [ShowTitleOnSideBar] BIT NOT NULL Default 1, 
+    [LegalName] NVARCHAR(500) NULL, 
+    [GstNumber] NVARCHAR(500) NULL, 
+    [InvoicePhone] NVARCHAR(500) NULL, 
+    [InvoiceLogoImage] NVARCHAR(MAX) NULL, 
+    [InvoiceWebsite] NVARCHAR(500) NULL
 )
