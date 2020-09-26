26a4c9afc02e4f56d3877882abb1ecc41ac9ee2f
somayehgh <somayehghazvinian@gmail.com>
2020-09-26 12:01:32 -0700

    added custmoer statement table

---
+++ ecommerce/ecommerce/CustomerStatementSetting.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CustomerStatementSetting]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [EmailSubject] NVARCHAR(500) NOT NULL, 
+    [EmailAttachmentFileName] NVARCHAR(500) NOT NULL, 
+    [EmailBody] NVARCHAR(MAX) NOT NULL, 
+    [CCEmailAddress] NVARCHAR(500) NOT NULL
+)
