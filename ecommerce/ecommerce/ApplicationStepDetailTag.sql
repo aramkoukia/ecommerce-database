cd96bcfccdd5458bb65dbe907a923b8a28f57eaa
aramkoukia <aramkoukia@gmail.com>
2021-03-14 17:57:27 -0700

    adding new table

--- ecommerce/ecommerce/ApplicationStepDetailTag.sql
+++ ecommerce/ecommerce/ApplicationStepDetailTag.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ApplicationStepDetailTag]
 (
-	[ApplicationStepDetailTagId] INT NOT NULL PRIMARY KEY, 
+	[ApplicationStepDetailTagId] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [ApplicationStepDetailId] INT NOT NULL,
 	[TagId] INT NOT NULL,
     CONSTRAINT [FK_ApplicationStepDetailTag_ToApplicationStepDetail] FOREIGN KEY (ApplicationStepDetailId) REFERENCES [ApplicationStepDetail](ApplicationStepDetailId),
